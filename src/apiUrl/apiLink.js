6b612f648ba16b6e984f73e48f25069999ad1ad4
Zubenna <emelunna@gmail.com>
2021-06-08 21:46:45 +0100

    Added cors anywhere url to api request

--- src/apiUrl/apiLink.js
+++ src/apiUrl/apiLink.js
@@ -1,3 +1,4 @@
-const url = 'https://stormy-badlands-31674.herokuapp.com/api/v1';
+// https://shrouded-anchorage-43717.herokuapp.com/
+const url = 'https://shrouded-anchorage-43717.herokuapp.com/https://stormy-badlands-31674.herokuapp.com/api/v1';
 
 export default url;
